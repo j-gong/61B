fa193e297f01aaeebcabb916cd848fc07307b9d8
Grant Kehn <gkehn@berkeley.edu>
2018-03-06 08:40:24 -0800

    please

--- proj2/byog/Core/Screen.java
+++ proj2/byog/Core/Screen.java
@@ -1,12 +1,10 @@
 package byog.Core;
 
 import byog.TileEngine.TETile;
-import byog.TileEngine.Tileset;
 import edu.princeton.cs.introcs.StdDraw;
 import java.awt.Font;
 import java.awt.Color;
 import java.io.Serializable;
-import java.util.Random;
 
 public class Screen implements Serializable {
      int width;
@@ -16,7 +14,6 @@ public class Screen implements Serializable {
 
      String prevAction;
 
-    //Screen might need to take in a seed?
     Screen() {
     }
 
@@ -51,20 +48,15 @@ public class Screen implements Serializable {
 
     void intro() {
         StdDraw.setPenColor(Color.CYAN);
-        StdDraw.text(10, game.HEIGHT - 3, "Some teenage delinquents have been");
-        StdDraw.text(13, game.HEIGHT - 5, "defacing public property. Apprehend them!");
+        StdDraw.text(game.WIDTH, game.HEIGHT - 3, "Some teenage delinquents have been");
+        StdDraw.text(game.WIDTH, game.HEIGHT - 5, "defacing public property. Apprehend them!");
         StdDraw.setPenColor(Color.YELLOW);
         StdDraw.show();
     }
 
     //will run this method while !gameover
     void drawHUD(){
-            /*
-            Font smallFont = new Font("Monaco", Font.BOLD, 20);
-            StdDraw.setFont(smallFont);*/
             StdDraw.setPenColor(Color.YELLOW);
-            //StdDraw.line(0, height -12 , width, height - 12);
-            //show below depends on whether the next while loops stays
             StdDraw.show();
 
     }
@@ -73,10 +65,10 @@ public class Screen implements Serializable {
 
 
         StdDraw.setPenColor(Color.YELLOW);
-        //StdDraw.text(game.WIDTH - this.width/5, game.HEIGHT - 9, this.mousepoint());
-        StdDraw.text(game.WIDTH - game.WIDTH / 7, game.HEIGHT - 9, "energy: " + game.robocop.energy);
-        StdDraw.text(game.WIDTH - game.WIDTH / 7, game.HEIGHT - 14,"Minutes before night: " + game.sunlight);
-        StdDraw.text(game.WIDTH - game.WIDTH / 7, game.HEIGHT - 19,"Weapon: " + game.robocop.weapon.name + ". Uses: " + game.robocop.weapon.uses);
+        StdDraw.text(game.WIDTH / 12, game.HEIGHT - 9, "energy: " + game.robocop.energy);
+        StdDraw.text(game.WIDTH / 12, game.HEIGHT - 14,"sunlight: " + game.sunlight);
+        StdDraw.text(game.WIDTH / 12, game.HEIGHT - 19,"weapon: " + game.robocop.weapon.name);
+        StdDraw.text(game.WIDTH / 12, game.HEIGHT - 21, "uses: " + game.robocop.weapon.uses);
         StdDraw.show();
 
         showPrev();
@@ -90,9 +82,9 @@ public class Screen implements Serializable {
     private void showVandals() {
         for (int i = 0; i < game.criminals.length; i += 1) {
             if (!game.criminals[i].caught) {
-                StdDraw.text(game.WIDTH - game.WIDTH / 7, 17 - (i * 2), "Vandal" + i + ": ▲");
+                StdDraw.text(game.WIDTH / 12, 17 - (i * 2), "Vandal" + i + ": ▲");
             } else {
-                StdDraw.text(game.WIDTH - game.WIDTH / 7, 17 - (i * 2), "Vandal" + i + ": ❀");
+                StdDraw.text(game.WIDTH / 12, 17 - (i * 2), "Vandal" + i + ": ❀");
             }
             StdDraw.show();
         }
@@ -100,32 +92,37 @@ public class Screen implements Serializable {
 
     void showMousePoint() {
         StdDraw.setPenColor(Color.BLACK);
-        StdDraw.filledCircle(game.WIDTH - game.WIDTH / 7, game.HEIGHT - 4, 4);
+        StdDraw.filledEllipse(game.WIDTH / 12, game.HEIGHT - 4, 6, 4);
         StdDraw.setPenColor(Color.YELLOW);
-        StdDraw.text(game.WIDTH - game.WIDTH / 7, game.HEIGHT - 4, this.mousepoint());
+        StdDraw.text(game.WIDTH / 12, game.HEIGHT - 4, this.mousepoint());
         StdDraw.show(); }
 
     private String mousepoint() {
-            int x = (int) StdDraw.mouseX();
+            int x = (int) StdDraw.mouseX() - 12;
             int y = (int) StdDraw.mouseY();
-            if (game.WORLD[x][y].equals(Tileset.WALL)) {
-                return "Wall" + Integer.toString(x) + Integer.toString(y);
-            } else if (game.WORLD[x][y].equals(Tileset.PLAYER)) {
-                return "Dats you" + Integer.toString(x) + Integer.toString(y);
-            } else if (game.WORLD[x][y].equals(Tileset.FLOOR)) {
-                return "Floor" + Integer.toString(x) + Integer.toString(y);
-            } else if (game.WORLD[x][y].equals(Tileset.NOTHING)) {
-                return " " + Integer.toString(x) + Integer.toString(y);
-            } else if (game.WORLD[x][y].equals(Tileset.MOUNTAIN)) {
-                return "vandal " + Integer.toString(x) + Integer.toString(y);
-            } else if (game.WORLD[x][y].equals(Tileset.GRASS)) {
-                return "energy pack " + Integer.toString(x) + Integer.toString(y);
-            } else if (game.WORLD[x][y].equals(Tileset.SAND)) {
-                return "sock launcher " + Integer.toString(x) + Integer.toString(y);
-            } else if (game.WORLD[x][y].equals(Tileset.WATER)) {
-                return "roller blades " + Integer.toString(x) + Integer.toString(y);
+            try {
+                TETile check = game.WORLD[x][y];
+                if (check.description().equals("wall")) {
+                    return "Wall" + Integer.toString(x) + Integer.toString(y);
+                } else if (check.description().equals("player")) {
+                    return "Dats you" + Integer.toString(x) + Integer.toString(y);
+                } else if (check.description().equals("floor")) {
+                    return "Floor" + Integer.toString(x) + Integer.toString(y);
+                } else if (check.description().equals("nothing")) {
+                    return " " + Integer.toString(x) + Integer.toString(y);
+                } else if (check.description().equals("mountain")) {
+                    return "vandal " + Integer.toString(x) + Integer.toString(y);
+                } else if (check.description().equals("grass")) {
+                    return "energy pack " + Integer.toString(x) + Integer.toString(y);
+                } else if (check.description().equals("sand")) {
+                    return "sock launcher " + Integer.toString(x) + Integer.toString(y);
+                } else if (check.description().equals("water")) {
+                    return "roller blades " + Integer.toString(x) + Integer.toString(y);
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.print(" ");
             }
-        return " ";
+        return " " + Integer.toString(x) + Integer.toString(y);
     }
 
     void gameover() {
@@ -168,8 +165,6 @@ public class Screen implements Serializable {
         }
     }
 
-        //TODO: make all the tiles darker shades -> make the message show up as well, getting rendered over
-
     void screenUse(String weaponName) {
         prevAction = weaponName + " used";
     }
@@ -179,8 +174,40 @@ public class Screen implements Serializable {
             prevAction = " ";
         }
         StdDraw.setPenColor(Color.CYAN);
-        StdDraw.text(game.WIDTH - game.WIDTH / 7, game.HEIGHT - 22, prevAction);
+        StdDraw.text(game.WIDTH / 12, game.HEIGHT - 27, prevAction);
         StdDraw.setPenColor(Color.YELLOW);
     }
 
+    /**
+     * Contains constant tile objects, to avoid having to remake the same tiles in different parts of
+     * the code.
+     *
+     * You are free to (and encouraged to) create and add your own tiles to this file. This file will
+     * be turned in with the rest of your code.
+     *
+     * Ex:
+     *      world[x][y] = Tileset.FLOOR;
+     *
+     * The style checker may crash when you try to style check this file due to use of unicode
+     * characters. This is OK.
+     */
+
+     public static class Tileset implements Serializable {
+         public static TETile PLAYER = new TETile('@', Color.white, Color.black, "player");
+         public static TETile WALL = new TETile('#', new Color(216, 128, 128), Color.darkGray,
+                "wall");
+         public static  TETile FLOOR = new TETile('·', new Color(128, 192, 128), Color.black,
+                "floor");
+         public static final  TETile NOTHING = new TETile(' ', Color.black, Color.black, "nothing");
+         public static final  TETile GRASS = new TETile('"', Color.green, Color.black, "grass");
+         public static final  TETile WATER = new TETile('≈', Color.blue, Color.black, "water");
+         public static final  TETile FLOWER = new TETile('❀', Color.magenta, Color.pink, "flower");
+         public static final  TETile LOCKED_DOOR = new TETile('█', Color.orange, Color.black,
+                "locked door");
+         public static final  TETile UNLOCKED_DOOR = new TETile('▢', Color.orange, Color.black,
+                "unlocked door");
+         public static final  TETile SAND = new TETile('▒', Color.yellow, Color.black, "sand");
+         public static final  TETile MOUNTAIN = new TETile('▲', Color.gray, Color.black, "mountain");
+         public static final  TETile TREE = new TETile('♠', Color.green, Color.black, "tree");
+    }
 }
